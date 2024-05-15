151c2be2bca7896d05c2b758983b8635fdad637e
Brendan <brendanalex212@gmail.com>
2024-05-15 16:59:21 +0700

    Initial Commit

---
+++ PanasDingin Watch App/PanasDinginApp.swift
@@ -0,0 +1,17 @@
+//
+//  PanasDinginApp.swift
+//  PanasDingin Watch App
+//
+//  Created by Brendan Alexander Soendjojo on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PanasDingin_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
